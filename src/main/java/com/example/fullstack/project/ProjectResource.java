2a63e94d59dae11c6b81b020bd2a53a8aeb86b05
Raphael Pizzo <raphael.pizzo@pizzosoft.com.br>
2022-12-16 17:16:30 -0300

    Initial commit

---
+++ src/main/java/com/example/fullstack/project/ProjectResource.java
@@ -0,0 +1,48 @@
+package com.example.fullstack.project;
+
+import com.example.fullstack.project.Project;
+import com.example.fullstack.project.ProjectService;
+import io.smallrye.mutiny.Uni;
+import org.jboss.resteasy.reactive.ResponseStatus;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/api/v1/projects")
+public class ProjectResource {
+
+    private final ProjectService projectService;
+
+    @Inject
+    public ProjectResource(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
+    @GET
+    public Uni<List<Project>> get() {
+        return projectService.listForUser();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @ResponseStatus(201)
+    public Uni<Project> create(Project project) {
+        return projectService.create(project);
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("{id}")
+    public Uni<Project> update(@PathParam("id") long id, Project project) {
+        project.id = id;
+        return projectService.update(project);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public Uni<Void> delete(@PathParam("id") long id) {
+        return projectService.delete(id);
+    }
+}
